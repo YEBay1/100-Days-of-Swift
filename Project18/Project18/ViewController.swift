dd8e0ccd1543d2a58258ed02224cd2a93694ce7c
YEBay <104377308+YEBay1@users.noreply.github.com>
2022-12-10 13:14:56 +0300

    Add files via upload

---
+++ Project18/Project18/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by Yunus Emre Bayezit on 4.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for i in 1 ... 100 {
+            print("Got number \(i)")
+        }
+    }
+}
